755daef5584274070338e3e3c02268b7e953b97b
Wanxuan Li <wl767@cornell.edu>
2024-10-22 21:10:40 -0400

    Updated Welcome Page

--- frontend/src/components/WelcomeMid.js
+++ frontend/src/components/WelcomeMid.js
@@ -1,8 +1,9 @@
-
 import React from 'react';
 import { useNavigate } from 'react-router-dom'; 
 import '../App.css';
-import chatbot from '../img/chatbot.svg';  
+import chatIcon from '../img/chat.svg'; 
+import bookIcon from '../img/book.svg';
+import navIcon from '../img/nav.svg';
 
 const WelcomeMid = () => {
   const navigate = useNavigate(); 
@@ -14,18 +15,66 @@ const WelcomeMid = () => {
   };
 
   return (
-    <div className="welcome-mid">
-      <div className="welcome-content">
-        <h2>My Smart Memory</h2>
-        <p>My Smart Memory is a personal assistant that saves and interacts with web content efficiently.</p>
+    <div className="intro-container">
+      <div className="intro-content">
+      <h2>My Smart Memory</h2>
+      <p>My Smart Memory is a personal assistant that saves and interacts with web content efficiently.</p>
+        <div className="intro-features">
+          <div className="feature">
+            <div style={{
+              width: '140px',
+              height: '140px',
+              backgroundColor: 'white',
+              borderRadius: '50%',
+              display: 'flex',
+              justifyContent: 'center',
+              alignItems: 'center',
+              margin: '0 auto 20px auto'
+            }}>
+              <img src={navIcon} alt="Library Icon" style={{ width: '58%' }} />
+            </div>
+            <p style={{ fontWeight: 'bold' }}>Your personal content library</p>
+            <p>Save and store important web content easily.</p>
+          </div>
+          
+          <div className="feature">
+            <div style={{
+              width: '140px',
+              height: '140px',
+              backgroundColor: 'white',
+              borderRadius: '50%',
+              display: 'flex',
+              justifyContent: 'center',
+              alignItems: 'center',
+              margin: '0 auto 20px auto'
+            }}>
+              <img src={bookIcon} alt="Search Icon" style={{ width: '60%' }} />
+            </div>
+            <p style={{ fontWeight: 'bold' }}>Instant search results</p>
+            <p>Quickly find answers within your saved content.</p>
+          </div>
+          
+          <div className="feature">
+            <div style={{
+              width: '140px',
+              height: '140px',
+              backgroundColor: 'white',
+              borderRadius: '50%',
+              display: 'flex',
+              justifyContent: 'center',
+              alignItems: 'center',
+              margin: '0 auto 20px auto'
+            }}>
+              <img src={chatIcon} alt="Assistant Icon" style={{ width: '65%', marginTop:'10px'}} />
+            </div>
+            <p style={{ fontWeight: 'bold' }}>Tailored personal assistant</p>
+            <p>Get customized answers based on your preferences.</p>
+          </div>
+        </div>
         <button className="start-button" onClick={handleStartClick}>Start</button> 
-        
-
       </div>
-      <img src={chatbot} alt="Chatbot Icon" className="chatbot-image" /> 
     </div>
   );
 };
 
 export default WelcomeMid;
-
--- frontend/src/pages/WelcomePage.js
+++ frontend/src/pages/WelcomePage.js
@@ -2,7 +2,6 @@ import React from 'react';
 import Nav from '../components/Nav';
 import WelcomeMid from '../components/WelcomeMid';
 import Footer from '../components/Footer';
-import Intro from '../components/Intro';
 import '../App.css';
 
 const WelcomePage = () => {
@@ -10,7 +9,6 @@ const WelcomePage = () => {
     <div className="welcome-page">
       <Nav />
       <WelcomeMid /> 
-      <Intro /> 
       <Footer />
     </div>
   );
