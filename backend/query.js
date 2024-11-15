3db42b47638f188cf5816db85ac71bf8342d6d52
killerwhaleskadi <112710549+killerwhaleskadi@users.noreply.github.com>
2024-11-15 03:10:54 -0500

    database connection

--- backend/query.js
+++ backend/query.js
@@ -8,7 +8,7 @@ async function getData() {
   
       // Perform a query
       const result = await poolConnection.request().query('SELECT * FROM Users');
-      await poolConnection.request().query("INSERT INTO Users(username, password, email) VALUES ('Menghan', '12345', 'mx253@cornell.edu')");
+      // await poolConnection.request().query("INSERT INTO Users(username, password, email) VALUES ('Menghan', '12345', 'mx253@cornell.edu')");
       console.log('Data from your table:', result.recordset);
     } catch (err) {
       console.error('Error querying the database:', err);
