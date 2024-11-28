a58e68188274fb849dcad5b790100101f48bc863
YW-2022 <97448262+YW-2022@users.noreply.github.com>
2024-11-28 17:26:47 -0500

    Redux - light and dark. Clean code.

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,42 +1,9 @@
 
-// import React from 'react';
-// import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-// import WelcomePage from './pages/WelcomePage';
-// import SignIn from './pages/SignIn';
-// import PersonalHome from './pages/PersonalHome';
-// import RegistrationPage from './pages/RegistrationPage';
-// import URLSubmission from './pages/URLSubmissionPage';
-// import URLSubmission2 from './pages/URLSubmissionPage2';
-// import ChatPage from './pages/ChatPage';
-// import ChatHistoryPage from './pages/ChatHistoryPage';
-
-// function App() {
-//   return (
-//     <Router>
-//       <div className="App">
-//         <Routes>     
-//           <Route path="/welcome" element={<WelcomePage />} />
-//           <Route path="/signin" element={<SignIn />} />
-//           <Route path="/personal-home" element={<PersonalHome />} />
-//           <Route path="/signup" element={<RegistrationPage />} />
-//           {/* <Route path="/add-url" element={<URLSubmission />} /> */}
-//           <Route path="/add-url-2" element={<URLSubmission2 />} />
-//           <Route path="/chatpage" element={<ChatPage />} />
-//           <Route path="/chathistorypage" element={<ChatHistoryPage />} />
-
-//         </Routes>
-//       </div>
-//     </Router>
-//   );
-// }
-
-// export default App;
-
 
 import React, { useEffect } from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-import { Provider } from 'react-redux'; // 引入 Redux Provider
-import { useSelector } from 'react-redux'; // 获取 Redux 状态
+import { Provider } from 'react-redux'; 
+import { useSelector } from 'react-redux'; 
 
 import WelcomePage from './pages/WelcomePage';
 import SignIn from './pages/SignIn';
@@ -45,12 +12,11 @@ import RegistrationPage from './pages/RegistrationPage';
 import URLSubmission2 from './pages/URLSubmissionPage2';
 import ChatPage from './pages/ChatPage';
 import ChatHistoryPage from './pages/ChatHistoryPage';
-import { store } from './store'; // 引入配置好的 Redux Store
+import { store } from './store'; 
 
 function AppContent() {
-  const themeMode = useSelector((state) => state.theme.mode); // 获取 Redux 中的主题状态
+  const themeMode = useSelector((state) => state.theme.mode); 
 
-  // 根据 Redux 中的主题动态设置 body 样式
   useEffect(() => {
     document.body.className = themeMode === 'dark' ? 'dark-mode' : 'light-mode';
   }, [themeMode]);
