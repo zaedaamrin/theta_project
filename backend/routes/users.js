46d9c4689a6d90b133d8dd582b5412f48f2a0485
Zaeda Amrin <71987621+zaedaamrin@users.noreply.github.com>
2024-12-05 12:08:31 -0500

    Add test route to users.js

--- backend/routes/users.js
+++ backend/routes/users.js
@@ -3,10 +3,9 @@ const { userController } = require("../controllers/users.js");
 
 const router = new Router();
 
-// router.get('/api/users', userController.getUsers);
-// router.post('/api/users', userController.postUser);
-
-// router.get('/api/users/:userId', userController.getUser);
+app.get('/api/testuser', (req, res) => {
+    res.send('user route successful');
+  });
 
 router.post('/api/users/signin', userController.signInUser);
 router.post('/api/users/signup', userController.signUpUser);
